9679a634373c01432fdbc140880c454351b1aa15
sr6062a <rai05111999@gmail.com>
2026-03-13 03:05:05 +0530

    UC1:Application Entry & Welcome Message

---
+++ src/HotelBookingApp.java
@@ -0,0 +1,9 @@
+public class HotelBookingApp {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+    }
+}
